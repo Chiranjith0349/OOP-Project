56d5b8ea7136fd988151731945cdda257dcdfd8e
Akshay-Arun-Iyer <114094920+Akshay-Arun-Iyer@users.noreply.github.com>
2024-12-02 20:25:20 +0400

    Update Login.java

--- Documents/NetBeansProjects/JavaApplication1/src/ia/Login.java
+++ Documents/NetBeansProjects/JavaApplication1/src/ia/Login.java
@@ -12,10 +12,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Chiru
- */
+
 public class Login extends javax.swing.JFrame {
 
     /**
